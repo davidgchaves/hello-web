e2125c73d4ef7c2ccdded43d15bb5330108543a0
David G Chaves <davidgchaves@gmail.com>
2014-05-16 17:21:00 +0200

    Greet responds to "/" and 404 (page not found) otherwise

--- src/hello_web/core.clj
+++ src/hello_web/core.clj
@@ -2,9 +2,13 @@
   (:require [ring.adapter.jetty :as jetty]))
 
 (defn greet [req]
-  {:status 200
-   :body "Hello, web!"
-   :headers {}})
+  (if (= "/" (:uri req))
+    {:status 200
+     :body "Hello, web!"
+     :headers {}}
+    {:status 404
+     :body "Page not found."
+     :headers {}}))
 
 (defn -main [port]
   (jetty/run-jetty greet

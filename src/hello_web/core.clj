2226c191656bc7ee4bf54d1355522a297745da5c
David G Chaves <davidgchaves@gmail.com>
2014-05-16 17:11:15 +0200

    Add a proper greet ring-handler

--- src/hello_web/core.clj
+++ src/hello_web/core.clj
@@ -1,7 +1,12 @@
 (ns hello-web.core
   (:require [ring.adapter.jetty :as jetty]))
 
+(defn greet [req]
+  {:status 200
+   :body "Hello, web!"
+   :headers {}})
+
 (defn -main [port]
-  (jetty/run-jetty (fn [req] {:status 200 :body "Hello, web!" :headers {}})
+  (jetty/run-jetty greet
                    {:port (Integer. port)}))
 
